c50ef821661dac673e5e3bccee2854735afec592
LabberDev <emruocco2@gmail.com>
2018-06-13 00:33:17 +0200

    add all get

--- terminal-manager.js
+++ terminal-manager.js
@@ -1,13 +1,13 @@
-var $ptty;
-    var $ptty = $('#terminal').Ptty({
-        i18n : {
-            welcome : '5D3 CHAIN VER 1.0.1 beta \nThe blockchain project\n\n',
-        },
-        ps: ">",
 
-        
+$ptty = $('#terminal').Ptty({
+    i18n : {
+        welcome : '5D3 CHAIN VER 1.0.1 beta \nThe blockchain project\n\n',
+    },
+    ps: ">",
 
-    });
+    
 
-    $ptty.echo('we tony')
-    //$ptty.run_command('clear'); //cancella quello scritto nel terminale
+});
+
+$ptty.echo('we tony')
+//$ptty.run_command('clear'); //cancella quello scritto nel terminale
